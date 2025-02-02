891fab946440c5e98b31db9cd3b0a62dd0a1c81c
Bryanow <bryan.ricardo7@gmail.com>
2025-02-02 17:02:54 -0300

    adding number of indicators update

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,15 @@
 const arrowForward = document.getElementById('arrow-forward');
 const arrowBack = document.getElementById('arrow-back');
-const conterBar = document.getElementById('counter-bar')
-const cars = document.querySelectorAll('.item');
+const conterBar = document.getElementById('counter-bar');
+const cars = document.querySelectorAll('.list .item');
 const bars = document.querySelectorAll('.bar')
+const indicator = document.querySelector('.indicators')
 let actualCar = 0;
 let actualBar = 0;
 
 arrowForward.addEventListener('click', function () {
     hidePreviousCar();//esconde o carro anterior
-    const selectedBar = document.querySelector('.active')
-    selectedBar.classList.remove('active')
+    hidePreviousBar();//esconde a barra anterior
 
     //loop dos carros
     if (actualCar === cars.length - 1) {
@@ -23,14 +23,17 @@ arrowForward.addEventListener('click', function () {
     } else {
         actualBar++;
     }
+    indicator.querySelector('.number').innerHTML = '0' + (actualBar + 1);
 
     showNextCar();//exibe o carro seguinte
-    bars[actualBar].classList.add('active')
+    showNextBar();//exibe a barra seguinte
+
 });
 
 arrowBack.addEventListener('click', function () {
-    hidePreviousCar();
-
+    hidePreviousCar();//esconde o carro anterior
+    hidePreviousBar();//esconde a barra anterior
+    
     //loop dos carros
     if (actualCar === 0) {
         actualCar = cars.length - 1;
@@ -43,9 +46,20 @@ arrowBack.addEventListener('click', function () {
     } else {
         actualBar--;
     }
+    indicator.querySelector('.number').innerHTML = '0' + (actualBar + 1);
 
-    showNextCar();
+    showNextCar();//exibe o carro seguinte
+    showNextBar();//exibe a barra seguinte
 })
+
+function showNextBar() {
+    bars[actualBar].classList.add('active');
+}
+
+function hidePreviousBar() {
+    const selectedBar = document.querySelector('.active');
+    selectedBar.classList.remove('active');
+}
 
 function showNextCar() {
     cars[actualCar].classList.add('active');
